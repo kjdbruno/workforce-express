54de83fff42d49c62558625397857e818f8aa48c
Kenneth Jay Bruno <61215678+kjdbruno@users.noreply.github.com>
2026-07-27 12:41:38 +0800

    Update server.js

--- server.js
+++ server.js
@@ -328,16 +328,18 @@ const io = socketIo(server, {
 });
 
 // Middleware
-app.use('*', cors({
-  // origin: '*', // Change this to your frontend's origin http://localhost:9000
-  // origin: 'https://hris-ccmi.com',
-  origin: [
-    'https://hris-ccmi.com',
-    'https://portal.hris-ccmi.com'
-  ],
-  methods: ['GET', 'POST'],
-  // credentials: true, // Optional, if you need to support credentials
+app.use(cors({
+    origin: [
+        'https://hris-ccmi.com',
+        'https://portal.hris-ccmi.com'
+    ],
+    methods: ['GET','POST','PUT','DELETE','OPTIONS'],
+    allowedHeaders: ['Content-Type','Authorization'],
+    credentials: true
 }));
+
+app.options('*', cors());
+
 app.use(express.json({ limit: '100mb' }));
 app.use(express.urlencoded({ limit: '100mb', extended: true }));
 
